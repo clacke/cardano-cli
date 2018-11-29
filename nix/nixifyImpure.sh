67034c07014f03695dcd3922a8be8ad52fff29d1
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-11-30 05:52:18 +0800

    WIP: nix_paths

--- nix/nixifyImpure.sh
+++ nix/nixifyImpure.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell --pure -p bash coreutils git nix-prefetch-git -i bash
+#! nix-shell -p bash coreutils git nix nix-prefetch-git -i bash
 
 set -euo pipefail
 
@@ -7,7 +7,8 @@ cd "${BASH_SOURCE[0]%/*}"
 cd "$(git rev-parse --show-toplevel)"
 
 commit_pattern='^160000 (.{40}) '
-attrs=()
+unset attrs
+declare -A attrs
 for path_key in $(git config -l --name-only -f .gitmodules | grep -E '[.]path$'); do
   path=$(git config --get -f .gitmodules $path_key)
   if ! [[ $(git ls-files -s "$path") =~ $commit_pattern ]]; then continue; fi
@@ -18,16 +19,29 @@ for path_key in $(git config -l --name-only -f .gitmodules | grep -E '[.]path$')
   url_impure_gitlab_github=$(git config -f .gitmodules --get $url_key)
   url_impure_github=${url_impure_gitlab_github/git@gitlab.com:/https://gitlab.com/}
   url=${url_impure_github/git@github.com:/https://github.com/}
-  attrs+=("\"$path\": $(nix-prefetch-git "$url" "$rev")")
+  attrs+=( [$path]=$(nix-prefetch-git "$url" "$rev") )
+done
+
+unset paths
+paths=("${!attrs[@]}")
+
+unset nix_paths
+declare -A nix_paths
+
+for path in "${paths[@]}"; do
+  nix_paths+=([$path]="$(nix-build --no-out-link --argstr json "${attrs[$path]}" -E '{json}: with import <nixpkgs> {}; fetchgit (builtins.removeAttrs (builtins.fromJSON json) ["date"])')")
 done
 
 cat <<EOF
 {
-  $(
-    for attr in "${attrs[@]:0:$((${#attrs[@]}-1))}"; do
-      echo "${attr},"
-    done
-  )
-  ${attrs[@]:$((${#attrs[@]}-1))}
+  $(for path in "${paths[@]}"; do
+    echo "\"$path\": ${attrs[$path]},"
+  done)
+  "@nix_paths": {
+    $(for path in "${paths[@]:0:$((${#paths[@]}-1))}"; do
+      echo "\"$path\": \"${nix_paths[$path]}\","
+    done)
+    "${paths[-1]}": "${nix_paths[${paths[-1]}]}"
+  }
 }
 EOF
